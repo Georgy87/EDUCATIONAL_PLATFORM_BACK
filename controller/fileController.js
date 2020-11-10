c7b8c7a7c425fd6f00a1239ec2d80cd77a536116
GEORGY <goshana87@mail.ru>
2020-11-10 15:47:00 +0300

    file controller

--- controller/fileController.js
+++ controller/fileController.js
@@ -6,8 +6,23 @@ const Uuid = require("uuid");
 class FileController {
     async uploadFile(req, res) {
         try {
-            const file = req.files;
-            console.log(file);
+            const file = req.files.file;
+            const Path = path.join(__dirname, "../files");
+            file.mv(Path + "/" + file.name);
+
+            let files = await File.find({ user: req.user.id });
+
+            const repeateFilter = files.filter((files) => {
+                return files.name === file.name;
+            });
+
+            if(repeateFilter.length === 0) {
+                const dbFile = new File({
+                    name: file.name,
+                    user: req.user.id,
+                });
+                await dbFile.save();
+            }
         } catch (e) {
             console.log(e);
             return res.status(500).json({ message: "Upload error" });
