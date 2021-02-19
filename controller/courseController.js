a7aeda82189897fbd764bc4549f765325f6445af
GEORGY <goshana87@mail.ru>
2021-02-19 19:30:05 +0300

    Refactoring promises and error processing for course controller.

--- controller/courseController.js
+++ controller/courseController.js
@@ -62,7 +62,19 @@ class courseController {
                 ],
             });
 
-            await dbFile.save();
+            dbFile.save((err) => {
+                if (err) {
+                    return res.status(404).json({
+                        status: "Error upload course",
+                        message: err,
+                    });
+                }
+
+                res.json({
+                    status: "success",
+                    message: "Course upload done",
+                });
+            })
         } catch (e) {
             return res
                 .status(500)
@@ -72,29 +84,18 @@ class courseController {
 
     async getCourses(req, res) {
         try {
-            const courses = await TeacherCourse.find();
-            // const c = await TeacherCourse.find({"content._id": "5feb4590675e3e305ce140eb"});
+            TeacherCourse.find().exec((err, courses) => {
+                if (err) {
+                    return res.status(404).json({
+                        status: "Error get course",
+                        message: err,
+                    });
+                }
 
-            // const query = "5feb4590675e3e305ce140eb";
-            // await TeacherCourse.findOne(query, (error, doc) => {
-
-            //     // Do some mutations
-            //     doc.content.module = 'some new value'
-
-            //     // Pass in the mutated doc and replace
-            //     // MyModel.replaceOne(query, doc, (error, newDoc) => {
-            //     //      console.log('It worked!')
-            //     // })module
-
-            // })
-            // module.save()
-            // const course = await TeacherCourse.findOne({"content._id": "6003812df69ac10754e61cff"}).exec(function(err, data) {
-
-            //     console.log(data);
-            // });
-
-            await res.json({
-                courses,
+                return res.json({
+                    status: "success",
+                    courses,
+                });
             });
         } catch (e) {
             console.log(e);
@@ -103,14 +104,28 @@ class courseController {
 
     async deleteCourse(req, res) {
         try {
-            const course = await TeacherCourse.findOne({ _id: req.query.id });
-            const Path = path.join(
-                __dirname,
-                `../static/coursePhotos/${req.query.name}`
-            );
-            fs.unlinkSync(Path);
-            await course.remove();
-            return res.json({ message: "Coure was delete" });
+            TeacherCourse.findOne({ _id: req.query.id }).exec((err, course) => {
+                if (err) {
+                    return res.status(400).json({
+                        status: "Error course delete",
+                        message: err,
+                    });
+                }
+
+                const Path = path.join(
+                    __dirname,
+                    `../static/coursePhotos/${req.query.name}`
+                );
+
+                fs.unlinkSync(Path);
+
+                course.remove();
+
+                return res.json({
+                    status: "success",
+                    message: "Course was deleted"
+                });
+            });
         } catch (e) {
             console.log(e);
             return res.status(500).json({ message: "Delete course error" });
@@ -120,23 +135,15 @@ class courseController {
     async getProfileCourse(req, res) {
         try {
             const id = req.query.id;
-
-            let courses = await TeacherCourse.find();
-
-            const courseProfile = courses.filter((course) => course._id == id);
-            const userId = courseProfile[0].user;
-
-            const user = await User.findOne({ _id: userId });
-
-            courseProfile[0].competence = user.competence;
-            // const course = await TeacherCourse.findOne({ user: userId });
-            // course.save();
-            await TeacherCourse.updateMany(
-                { user: userId },
-                { $set: { competence: user.competence, avatar: user.avatar } }
-            );
-
-            await res.json(courseProfile[0]);
+            TeacherCourse.findOne({ _id: id }).populate('user').exec((err, course) => {
+                if (err) {
+                    return res.status(404).json({
+                        status: "Course profile not found",
+                        message: err,
+                    });
+                }
+                return res.json(course);
+            });
         } catch (e) {
             console.log(e);
         }
@@ -146,28 +153,35 @@ class courseController {
         try {
             const { text } = req.body;
 
-            await TeacherCourse.findOne({
+            TeacherCourse.findOne({
                 _id: req.query.courseId,
             })
                 .select(
                     "-content -smallDescription -fullDescription -profession -competence -user -author -price -__v"
                 )
-                .exec(async function(err, course) {
+                .exec(async function (err, course) {
                     course.comments.unshift({
                         text: text,
                         user: req.user.id,
                     });
 
-                    course.save();
+                    course.save(async (err) => {
+                        if (err) {
+                            return res.status(400).json({
+                                status: 'Create comment error',
+                                message: err
+                            });
+                        }
 
-                    const data = await course
-                        .populate("comments.user")
-                        .populate("comments.comments.user")
-                        .execPopulate();
+                        const data = await course
+                            .populate("comments.user")
+                            .populate("comments.comments.user")
+                            .execPopulate();
 
-                    return res.json({
-                        status: "SUCCESS",
-                        data: data,
+                        return res.json({
+                            status: "success",
+                            data: data,
+                        });
                     });
                 });
         } catch (e) {
@@ -181,7 +195,7 @@ class courseController {
             const { courseId, commentId } = req.query;
             const userId = req.user.id;
 
-            await TeacherCourse.findOne({ _id: courseId }).exec(function(err, course) {
+            TeacherCourse.findOne({ _id: courseId }).exec(function (err, course) {
                 course.comments.map(async (el) => {
                     if (el._id.toString() === commentId) {
                         el.comments.push({
@@ -189,19 +203,26 @@ class courseController {
                             user: userId,
                         });
 
-                        course.save();
-
-                        const data = await course
-                            .populate("comments.user")
-                            .populate("comments.comments.user")
-                            .execPopulate();
-
-                        await data.comments.map((el) => {
-                            if (el._id.toString() === commentId) {
-                                return res.json({
-                                    data: el,
+                        course.save(async (err) => {
+                            if (err) {
+                                return res.status(400).json({
+                                    status: 'Create comment error',
+                                    message: err
                                 });
                             }
+
+                            const data = await course
+                                .populate("comments.user")
+                                .populate("comments.comments.user")
+                                .execPopulate();
+
+                            data.comments.map((el) => {
+                                if (el._id.toString() === commentId) {
+                                    return res.json({
+                                        data: el,
+                                    });
+                                }
+                            });
                         });
                     }
                 });
@@ -214,10 +235,10 @@ class courseController {
     async getCommentsForCourse(req, res) {
         try {
             const { courseId } = req.query;
-            await TeacherCourse.findOne({ _id: courseId })
+            TeacherCourse.findOne({ _id: courseId })
                 .populate("comments.user")
                 .populate("comments.comments.user")
-                .exec(function(err, data) {
+                .exec(function (err, data) {
                     if (data) {
                         return res.json({
                             data: data.comments,
@@ -232,10 +253,10 @@ class courseController {
     async getReplyToComment(req, res) {
         try {
             const { courseId, commentId } = req.query;
-            await TeacherCourse.findOne({ _id: courseId })
+            TeacherCourse.findOne({ _id: courseId })
                 .populate("comments.user")
                 .populate("comments.comments.user")
-                .exec(function(err, course) {
+                .exec(function (err, course) {
                     course.comments.map((data) => {
                         if (data._id.toString() === commentId) {
                             return res.json({
@@ -252,19 +273,27 @@ class courseController {
     async getTeacherProfile(req, res) {
         try {
             const teacherId = req.query.teacherId;
-            const user = await User.findOne({ _id: teacherId });
-            const allTeacherCourses = await TeacherCourse.find({
-                user: { $in: teacherId },
-            });
+            User.findOne({ _id: teacherId }).exec((err, user) => {
+                if (err) {
+                    return res.status(400).json({
+                        status: 'Get teacher profile error',
+                        message: err
+                    });
+                }
 
-            await res.json({
-                avatar: user.avatar,
-                email: user.email,
-                id: user.id,
-                name: user.name,
-                surname: user.surname,
-                competence: user.competence,
-                courses: allTeacherCourses,
+                TeacherCourse.find({
+                    user: { $in: teacherId },
+                }).exec((err, allTeacherCourses) => {
+                    return res.json({
+                        avatar: user.avatar,
+                        email: user.email,
+                        id: user.id,
+                        name: user.name,
+                        surname: user.surname,
+                        competence: user.competence,
+                        courses: allTeacherCourses,
+                    });
+                });
             });
         } catch (error) {
             return res
@@ -280,13 +309,19 @@ class courseController {
 
             let totalPrice = 0;
 
-            await TeacherCourse.find({
+            TeacherCourse.find({
                 _id: { $in: ids },
             })
                 .select(
                     "-content -comments -user -smallDescription -fullDescription -updatedAt -__v -competence"
                 )
-                .exec(function(err, coursesDestructured) {
+                .exec(function (err, coursesDestructured) {
+                    if (err) {
+                        return res.status(400).json({
+                            status: 'Get courses for shopping cart error',
+                            message: err
+                        });
+                    }
                     coursesDestructured.map((element) => {
                         totalPrice += Number(element.price);
                     });
@@ -306,42 +341,47 @@ class courseController {
 
     async deleteCoursesForShoppingCart(req, res) {
         try {
-            const user = await User.findOne({ _id: req.user.id });
             const courseId = req.query.id;
-            const ids = user.shoppingCart;
+            User.findOne({ _id: req.user.id }).exec(async (err, user) => {
+                const ids = user.shoppingCart;
+                const UserCartShopids = ids.filter((el) => el != courseId);
+                user.shoppingCart = UserCartShopids;
 
-            const UserCartShopids = ids.filter((el) => el != courseId);
+                const courses = await TeacherCourse.find({
+                    _id: { $in: UserCartShopids },
+                });
 
-            user.shoppingCart = UserCartShopids;
+                let totalPrice = 0;
 
-            const courses = await TeacherCourse.find({
-                _id: { $in: UserCartShopids },
-            });
+                const coursesDestructured = courses.map((element) => {
+                    totalPrice += Number(element.price);
+                    return Object.assign(
+                        {},
+                        {
+                            photo: element.photo,
+                            author: element.author,
+                            price: element.price,
+                            smallDescription: element.smallDescription,
+                            profession: element.profession,
+                            _id: element._id,
+                        }
+                    );
+                });
 
-            let totalPrice = 0;
-
-            const coursesDestructured = courses.map((element) => {
-                totalPrice += Number(element.price);
-                return Object.assign(
-                    {},
-                    {
-                        photo: element.photo,
-                        author: element.author,
-                        price: element.price,
-                        smallDescription: element.smallDescription,
-                        profession: element.profession,
-                        _id: element._id,
+                user.save((err) => {
+                    if (err) {
+                        return res.status(404).json({
+                            status: "error",
+                            message: err,
+                        });
                     }
-                );
-            });
-
-            user.save();
-
-            return res.json({
-                coursesData: {
-                    coursesDestructured,
-                    totalPrice,
-                },
+                    return res.json({
+                        coursesData: {
+                            coursesDestructured,
+                            totalPrice,
+                        },
+                    });
+                });
             });
         } catch (error) {
             return res
@@ -352,21 +392,18 @@ class courseController {
 
     async getPurchasedCourses(req, res) {
         try {
-            const user = await User.findOne({ _id: req.user.id });
-            const ids = user.purchasedCourses;
-            const courses = await TeacherCourse.find({ _id: { $in: ids } });
-            const coursesDestructured = courses.map((element) => {
-                return Object.assign(
-                    {},
-                    {
-                        photo: element.photo,
-                        author: element.author,
-                        smallDescription: element.smallDescription,
-                        id: element._id,
+            User.findOne({ _id: req.user.id }).exec(async (err, user) => {
+                const ids = user.purchasedCourses;
+                TeacherCourse.find({ _id: { $in: ids } }).select( "-content -profession -competence -user -price -__v -comments -createdAt -updatedAt -avatar -fullDescription").exec((err, courses) => {
+                    if (err) {
+                        return res.status(400).json({
+                            status: 'Get purchased purses error',
+                            message: err
+                        });
                     }
-                );
+                    return res.json([...courses]);
+                });
             });
-            return res.json([...coursesDestructured]);
         } catch (error) {
             return res
                 .status(500)
@@ -376,9 +413,16 @@ class courseController {
 
     async getCourseForTraining(req, res) {
         try {
-            const course = await TeacherCourse.findOne({ _id: req.query.id });
+            TeacherCourse.findOne({ _id: req.query.id }).exec((err, course) => {
+                if (err) {
+                    return res.status(400).json({
+                        status: 'Get course for training error',
+                        message: err
+                    });
+                }
+                res.json({ course });
+            });
 
-            res.json({ course });
         } catch (error) {
             return res
                 .status(500)
@@ -389,42 +433,3 @@ class courseController {
 
 module.exports = new courseController();
 
-// async uploadCourse(req, res) {
-//     try {
-//         const {
-//             profession,
-//             author,
-//             price,
-//             smallDescription,
-//             fullDescription,
-//         } = req.body;
-
-//         const file = req.files.file;
-//         const Path = path.join(__dirname, `../static`);
-//         file.mv(Path + "/" + file.name);
-//         // console.log(Path + "/" + file.name);
-//         // let courses = await Course.find({ user: req.user.id });
-//         let courses = await Course.find();
-
-//         const repeateFilter = courses.filter((course) => {
-//             return course.name === file.name;
-//         });
-
-//         if (repeateFilter.length === 0) {
-//             const dbFile = new Course({
-//                 name: file.name,
-//                 // user: req.user.id,
-//                 profession: profession,
-//                 author: author,
-//                 price: price,
-//                 smallDescription: smallDescription,
-//                 fullDescription: fullDescription,
-//             });
-//             await dbFile.save();
-//             await res.json(dbFile);
-//         }
-//     } catch (e) {
-//         console.log(e);
-//         return res.status(500).json({ message: "Upload error" });
-//     }
-// }
